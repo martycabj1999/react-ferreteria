6c86cbbe0c564c8109359a048dead87659c1bdd9
ramiro-c <rami992009@gmail.com>
2020-02-12 16:57:24 -0300

    Agregue los componentes Dropdown de categoria, marca y rubro al navbar inferior

--- src/components/NavbarInferior.js
+++ src/components/NavbarInferior.js
@@ -1,25 +1,33 @@
 import React from 'react';
+import Dropdown from './Dropdown';
 
 const NavbarInferior = (props) => {
+    // Categorias, marcas y rubros
+    const { categories, brands, sections } = props;
 
-    const { categories } = props;
-    
     const listCategories = categories.map((category) =>
         <a href={category.url} className="dropdown-item">{category.name}</a>
     );
+
+    const listBrands = brands.map((brand) =>
+        <a href={brand.url} className="dropdown-item">{brand.name}</a>
+    );
     
+    const listSections = sections.map((section) =>
+        <a href={section.url} className="dropdown-item">{section.name}</a>
+    );
+
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <ul className="navbar-nav mt-n4">
                 <li className="nav-item">
-                    <div className="dropdown show">
-                        <button className="btn dropdown-toggle" id="categories" data-toggle="dropdown">
-                            Categorias
-                        </button>
-                        <div className="dropdown-menu" aria-labelledby="categories">
-                            {listCategories}
-                        </div>
-                    </div>
+                    <Dropdown show="Categorias" list={listCategories} />
+                </li>
+                <li className="nav-item">
+                    <Dropdown show="Marcas" list={listBrands} />
+                </li>
+                <li className="nav-item">
+                    <Dropdown show="Rubros" list={listSections} />
                 </li>
                 <li className="nav-item">
                     <a href="" className="nav-link">Contactanos</a>
