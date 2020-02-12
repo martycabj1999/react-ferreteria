866b9e9fffdef87f003fbfe35465eeae5c0e48a0
ramiro-c <rami992009@gmail.com>
2020-02-12 16:55:47 -0300

    Agregue los ejemplos de marcas y rubros

--- src/App.js
+++ src/App.js
@@ -6,28 +6,60 @@ import NavbarSuperior from "./components/NavbarSuperior";
 import NavbarInferior from "./components/NavbarInferior";
 
 function App() {
-  /* Categorias de ejemplo */
+  /* Listas de ejemplo */
+
+  // Categorias
   const categories = [
     {
-      name : "categoria 1",
-      url : "categoria_1"
+      name: "categoria 1",
+      url: "categoria_1"
     },
     {
-      name : "categoria 2",
-      url : "categoria_2"
+      name: "categoria 2",
+      url: "categoria_2"
     },
     {
-      name : "categoria 3",
-      url : "categoria_3"
+      name: "categoria 3",
+      url: "categoria_3"
+    }
+  ];
+  // Marcas
+  const brands = [
+    {
+      name: "marca 1",
+      url: "marca_1"
+    },
+    {
+      name: "marca 2",
+      url: "marca_2"
+    }
+  ];
+  // Rubros
+  const sections = [
+    {
+      name: "rubro 1",
+      url: "rubro_1"
+    },
+    {
+      name: "rubro 2",
+      url: "rubro_2"
+    },
+    {
+      name: "rubro 3",
+      url: "rubro_3"
+    },
+    {
+      name: "rubro 4",
+      url: "rubro_4"
     }
   ];
 
   return (
     <div>
       <header>
-              
-      <NavbarSuperior />
-      <NavbarInferior categories={categories}/>
+
+        <NavbarSuperior />
+        <NavbarInferior categories={categories} brands={brands} sections={sections} />
 
       </header>
     </div>
