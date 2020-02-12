9d72e67a12ae3c130e8d47eb82e83089ae991ef3
ramiro-c <rami992009@gmail.com>
2020-02-12 16:27:54 -0300

    Borre todo lo que venia por default en el componente App y en App.css

--- src/App.js
+++ src/App.js
@@ -23,24 +23,12 @@ function App() {
   ];
 
   return (
-    <div className="App">
-      
+    <div>
+      <header>
+              
       <NavbarSuperior />
       <NavbarInferior categories={categories}/>
 
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
