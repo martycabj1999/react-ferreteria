6b96e409ce722d7cae2ee57b8ea9e30cd6af0525
FlorenciaFried <florencia.d.fried@gmail.com>
2020-02-13 18:00:46 -0300

    Footer con popup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
 
 function App() {
+
+  const [modalShow, setModalShow] = React.useState(false);
+  const [modalName, setModalName] = useState('');
+
+  const sets = (name) => {
+    setModalShow(true);
+    setModalName(name);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <footer className="footer">
+          <div className="footer-container">
+
+                  <div onClick={() => sets('Ferretería')}> 
+                    &copy; Ferretería
+                  </div>
+
+                  <div onClick={() => sets('Políticas de privacidad')}>
+                    Políticas de privacidad
+                  </div>
+
+                  <div onClick={() => sets('FAQ')}>
+                    FAQ
+                  </div>
+
+                  <div onClick={() => sets('Términos y condiciones')}>
+                    Términos y condiciones
+                  </div>
+
+                  <Footer
+                    name={modalName}
+                    show={modalShow}
+                    onHide={() => setModalShow(false)}
+                  >
+                  </Footer>
+
+          </div>
+        </footer>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import {Modal, Button, Header} from 'react-bootstrap';
+
+const Footer = (props) => {
+
+    return(
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+
+            <Modal.Header closeButton>
+
+                <Modal.Title id="contained-modal-title-vcenter">
+                    {props.name}
+                </Modal.Title>
+
+            </Modal.Header>
+
+            <Modal.Body>
+                <p>
+                    Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+                    dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+                    consectetur ac, vestibulum at eros.
+                    Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+                    dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+                    consectetur ac, vestibulum at eros.
+                    Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+                    dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+                    consectetur ac, vestibulum at eros.
+                </p>
+
+            </Modal.Body>
+
+            <Modal.Footer>
+            <Button onClick={props.onHide}>Close</Button>
+            </Modal.Footer>
+
+      </Modal>
+
+    );
+}
+ 
+export default Footer;
