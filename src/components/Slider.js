27cc9905235c234bd222eef02ff453c3e4f18c15
ramiro-c <rami992009@gmail.com>
2020-02-16 02:25:45 -0300

    Componente Slider terminado

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -3,47 +3,42 @@ import { Carousel } from 'react-bootstrap';
 import './Slider.css';
 import firstSliderImage from './img/slider1.jpg';
 import secondSliderImage from './img/slider2.jpg';
-import thirdSliderImage from './img/slider3.jpg';
 
 const Slider = () => {
+
+    const items = [
+        {
+            src: firstSliderImage,
+            alt: 'Primer imagen',
+            title: 'Primer imagen',
+            caption: 'Nulla vitae elit libero, a pharetra augue mollis interdum.'
+        },
+        {
+            src: secondSliderImage,
+            alt: 'Segunda imagen',
+            title: 'Segunda imagen',
+            caption: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+        }
+    ];
+
+    const carouselItems = items.map((item) =>
+        <Carousel.Item>
+            <img
+                className="d-block w-100"
+                src={item.src}
+                alt={item.alt}
+                id="img"
+            />
+            <Carousel.Caption>
+                <h3>{item.title}</h3>
+                <p>{item.caption}</p>
+            </Carousel.Caption>
+        </Carousel.Item>
+    );
+
     return (
         <Carousel>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src={firstSliderImage}
-                    alt="First slide"
-                    id="img"
-                />
-                <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src={secondSliderImage}
-                    alt="Second slide"
-                    id="img"
-                />
-                <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src={thirdSliderImage}
-                    alt="Third slide"
-                    id="img"
-                />
-                <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
+            {carouselItems}
         </Carousel>
     )
 }
