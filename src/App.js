641fca8fb4489602a3d3cd5ba61c9335074d121b
Martin <martin@youmarket.com.ar>
2020-02-16 22:34:22 -0300

    Login, Register and 404

--- src/App.js
+++ src/App.js
@@ -1,69 +1,27 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Footer from './components/footer/Footer';
-import UpperNavbar from "./components/navbar/UpperNavbar";
-import LowerNavbar from "./components/navbar/LowerNavbar";
+import Layout from './components/layouts/Layout';
+import NotFound from "./components/NotFound";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
 
-  /* Listas de ejemplo */
-  // Categorias
-  const categories = [
-    {
-      title: "categoria 1",
-      url: "categoria_1"
-    },
-    {
-      title: "categoria 2",
-      url: "categoria_2"
-    },
-    {
-      title: "categoria 3",
-      url: "categoria_3"
-    }
-  ];
-  // Marcas
-  const brands = [
-    {
-      title: "marca 1",
-      url: "marca_1"
-    },
-    {
-      title: "marca 2",
-      url: "marca_2"
-    }
-  ];
-  // Rubros
-  const sections = [
-    {
-      title: "rubro 1",
-      url: "rubro_1"
-    },
-    {
-      title: "rubro 2",
-      url: "rubro_2"
-    },
-    {
-      title: "rubro 3",
-      url: "rubro_3"
-    },
-    {
-      title: "rubro 4",
-      url: "rubro_4"
-    }
-  ];
-
-  /* Usuario esta logueado o no */
-  const isLogued = false;
-
   return (
     <div>
-      <header>
-        <UpperNavbar />
-        <LowerNavbar categories={categories} brands={brands} sections={sections} isLogued={isLogued} />
-      </header>
-      <Footer />
+      <Router>
+        <Layout>
+          <Switch>
+            {/*<Route component={NotFound} />     */}     
+          </Switch>
+        </Layout>
+      </Router>
     </div>
   );
 }
