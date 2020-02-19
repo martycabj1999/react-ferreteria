740d2709a692f20b9c2d192138d364d768daad18
FlorenciaFried <florencia.d.fried@gmail.com>
2020-02-19 11:24:33 -0300

    Finalizando featured products

--- src/components/featured_products/FeaturedProducts.js
+++ src/components/featured_products/FeaturedProducts.js
@@ -7,59 +7,50 @@ const FeaturedProducts = () => {
     const minimunSlides=4;
     const featuredProducts = [
         {
-          name: "tornillos",
-          url_img: "https://images.pexels.com/photos/259988/pexels-photo-259988.jpeg?auto=compress&cs=tinysrgb&dpr=3",
+          name: "Hidrolavadora Profesional 135 Bar Motor ",
+          url_img: "https://images.pexels.com/photos/259988/pexels-photo-259988.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=100&w=100",
           price: 101
         },
         {
-          name: "tornillos",
-          url_img: "https://images.pexels.com/photos/909256/pexels-photo-909256.jpeg?auto=compress&cs=tinysrgb&dpr=2&",
+          name: "Hidrolavadora Profesional 135 Bar Motor ",
+          url_img: "https://images.pexels.com/photos/259988/pexels-photo-259988.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=100&w=100",
           price: 102
         },
         {
-          name: "tornillos",
-          url_img: "https://images.pexels.com/photos/1409215/pexels-photo-1409215.jpeg?auto=compress&cs=tinysrgb&dpr=2",
+          name: "Hidrolavadora Profesional 135 Bar Motor",
+          url_img: "https://images.pexels.com/photos/259988/pexels-photo-259988.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=100&w=100",
+          price: 103
         },
         {
-          name: "tornillos",
-          url_img: "https://images.pexels.com/photos/210881/pexels-photo-210881.jpeg?auto=compress&cs=tinysrgb&dpr=2",
+          name: "Hidrolavadora Profesional 135 Bar Motor ",
+          url_img: "https://images.pexels.com/photos/259988/pexels-photo-259988.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=100&w=100",
           price: 104
         },
         {
-          name: "tornillos",
-          url_img: "https://images.pexels.com/photos/1319460/pexels-photo-1319460.jpeg?auto=compress&cs=tinysrgb&dpr=2",
+          name: "Hidrolavadora Profesional 135 Bar Motor ",
+          url_img: "https://images.pexels.com/photos/259988/pexels-photo-259988.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=100&w=100",
           price: 105
         }
     ]
 
-    const listFeaturedProducts = featuredProducts.map((product) =>
-       
-     <Carousel.Item className="carosuel-item"> 
-        <Image thumbnail fluid
-          className="d-block w-50"
-          src={product.url_img} 
-        />
-
-        <h4>{product.price}</h4>
+    const listFeaturedProducts = featuredProducts.map((product) =>      
+      <div className='product'>
+        <Image thumbnail
+            src={product.url_img}
+        /> 
+        <h2>{product.price}</h2>
         <h6>{product.name}</h6>
         <Button variant="dark">Comprar</Button>
-      </Carousel.Item>
-    )
+      </div>
+    );
 
     return (
-        <Container className="featured-products-container">
-            <Row>
-                <Col xs={12} md={12}>
-                    <h3>Productos destacados</h3>
-                </Col>
-            </Row>
-            <Row>
-              <Carousel className="featured-products-carousel">
-                {listFeaturedProducts}
-              </Carousel>
-            </Row>
-            
-        </Container>
+      <div className="featured-products-container">
+        <h3>Productos destacados</h3>
+        <div class='scroll'>
+          {listFeaturedProducts}
+        </div>
+      </div>
     )
 }
 
