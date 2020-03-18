6e142c2d41b1c84563b0b2002529322c1972e59f
Martin <martin@youmarket.com.ar>
2020-03-17 22:22:29 -0300

    login and register

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -9,6 +9,19 @@ const AuthService = {
     formData.append('password', form.password);
 
     return http.post('login', formData);
+  },
+  register: (form) => {
+
+    const formData = new FormData();
+
+    formData.append('name', form.name);
+    formData.append('lastname', form.lastname);
+    formData.append('telephone', form.telephone);
+    formData.append('address', form.address);
+    formData.append('email', form.email);
+    formData.append('password', form.password);
+
+    return http.post('/user', formData);
   }
 }
 
