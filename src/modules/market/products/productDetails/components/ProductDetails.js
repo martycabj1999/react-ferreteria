fd2fa439fb5feb0955ce44328388bb35ff904a3a
FlorenciaFried <florencia.d.fried@gmail.com>
2020-03-25 16:19:16 -0300

    Product detail terminado, falta arreglar el estilo del componente featured products

--- src/modules/market/products/productDetails/components/ProductDetails.js
+++ src/modules/market/products/productDetails/components/ProductDetails.js
@@ -9,21 +9,26 @@ const ProductDetails = () => {
     const product = useSelector(state => state.products.productDetails);
 
     return (
-        
-        <div style={{width: 'auto'}}>
-            <div className="product-details-image">
-                <Image thumbnail style={{maxWidth:'200'}}
-                    src='https://www.mndelgolfo.com/wp-content/uploads/2017/01/martillo-truper-640x360.png'
-                />
-            </div>
-            <div className="product-details-body">
-                <h3>Producto: {product.name}</h3>
-                <h3>Precio: $ {product.price}</h3>
-                <h5>Descripción: {product.long_description}</h5>
-                <Button className="button" variant="dark">Comprar</Button>
+        <div>
+            <div style={{width: 'auto'}}>
+                <div className="product-details-image">
+                    <Image thumbnail style={{maxWidth:'200'}}
+                        src='https://www.mndelgolfo.com/wp-content/uploads/2017/01/martillo-truper-640x360.png'
+                    />
+                </div>
+                <div className="product-details-body">
+                    <h3>Producto: {product.name}</h3>
+                    <h3>Precio: $ {product.price}</h3>
+                    <h5>Descripción: {product.long_description}</h5>
+                    <Button className="button" variant="dark">Comprar</Button>
+                </div>
             </div>
 
+            <div>
+                <FeaturedProducts />
+            </div>
         </div>
+
     )
 }
 
