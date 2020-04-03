3a4d4ee1ead45ddbe46667f028a0700bfe10b71e
ramiro-c <rami992009@gmail.com>
2020-04-03 14:29:50 -0300

    Hice responsive el footer y le agregue un margen arriba para que no se junte con los otros elementos

--- src/modules/layouts/footer/pages/Footer.js
+++ src/modules/layouts/footer/pages/Footer.js
@@ -1,59 +1,61 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
+import Typography from '@material-ui/core/Typography'
+import { Container, Row } from 'react-bootstrap';
 import FooterPopup from '../components/FooterPopup';
 import '../styles/Footer.css';
-import Typography from '@material-ui/core/Typography'
 
 const Footer = () => {
 
-    const [modalShow, setModalShow] = useState(false);
-    const [modalName, setModalName] = useState('');
+  const [modalShow, setModalShow] = useState(false);
+  const [modalName, setModalName] = useState('');
 
-    const sets = (name) => {
-        setModalShow(true);
-        setModalName(name);
-    }
+  const sets = (name) => {
+    setModalShow(true);
+    setModalName(name);
+  };
 
-    const messages = useSelector(state => state.languages.messages);  
-    const currentColors = useSelector(state => state.customization.colors);  
+  const messages = useSelector(state => state.languages.messages);
+  const currentColors = useSelector(state => state.customization.colors);
 
-    return (
-      <div>
-        <footer style={{
-              backgroundColor: currentColors.colorPrimary
-              }} className="footer">
-          <Typography style={{
-            color: currentColors.textPrimary
-          }} />
-          
-            <div className="footer-container">
-
-              <div onClick={() => sets('Ferretería')}>
-                &copy; {messages['footer_business']}
-              </div>
-
-              <div onClick={() => sets('Políticas de privacidad')}>
-                {messages['footer_policies']}
-              </div>
-
-              <div onClick={() => sets('FAQ')}>
-                {messages['footer_faq']}
-              </div>
-
-              <div onClick={() => sets('Términos y condiciones')}>
-                {messages['footer_term_and_conditions']}
-              </div>
-
-              <FooterPopup
-                name={modalName}
-                show={modalShow}
-                onHide={() => setModalShow(false)}
-              />
-
-            </div>
-        </footer>
-      </div>
-    )
+  return (
+    <footer
+      style={{
+        backgroundColor: currentColors.colorPrimary,
+      }}
+      className="py-3 mt-3">
+      <Typography style={{
+        color: currentColors.textPrimary
+      }} />
+      <Container className="footer-container">
+        <Row>
+          <div onClick={() => sets('Ferretería')}>
+            &copy;{messages['footer_business']}
+          </div>
+        </Row>
+        <Row>
+          <div onClick={() => sets('Políticas de privacidad')}>
+            {messages['footer_policies']}
+          </div>
+        </Row>
+        <Row>
+          <div onClick={() => sets('FAQ')}>
+            {messages['footer_faq']}
+          </div>
+        </Row>
+        <Row>
+          <div onClick={() => sets('Términos y condiciones')}>
+            {messages['footer_term_and_conditions']}
+          </div>
+        </Row>
+      </Container>
+      <FooterPopup
+        name={modalName}
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+    </footer>
+  );
 }
 
 export default Footer;
